258c7d48f09459c4d2af3228ffb1ce9119b67ae7
TheMo-at <iam@themo.at>
2021-06-10 12:52:26 +0200

    Initial Commit

---
+++ BMI Rechner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Rechner
+//
+//  Created by Mario Mohar on 10.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
